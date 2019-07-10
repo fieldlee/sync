13d841556a5d2c0a06b7a2ac9c0864b0fa45656a
fieldlee <ldpfieldlee@gmail.com>
2019-07-09 19:52:42 -0700

    fix requirements

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask_restful import Api
+from flask_jsonrpc import JSONRPC
 from handle import CertPath,certfile,configfile,runCom,txfile,clone
 
 baasApp = Flask(__name__)
 baasApp.config['MAX_CONTENT_LENGTH'] = 60 * 1024 * 1024
 baasApp.config['UPLOAD_PATH'] = CertPath
 api = Api(baasApp)
+jsonrpc = JSONRPC(baasApp,'/api',enable_web_browsable_api=False)
+
+@jsonrpc.method('App.Index')
+def index():
+
+    return
+
 
 api.add_resource(certfile.UpCertFile,'/upcert')
 
@@ -18,4 +26,7 @@ api.add_resource(runCom.RunCom,'/runcom')
 api.add_resource(clone.Clone,'/clone')
 
 if __name__ == "__main__":
-    baasApp.run(debug=True,port=8000)
+    from werkzeug.contrib.fixers import ProxyFix
+    baasApp.wsgi_app = ProxyFix(baasApp.wsgi_app)
+
+    baasApp.run(debug=True,port=8000,host='0.0.0.0')
